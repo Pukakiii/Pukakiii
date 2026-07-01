907f00cbfb984e4fb91bb3b3325a51c18be8ae84
Pukakiii <pukaki.ip@gmail.com>
2026-07-02 01:37:47 +0200

    feat: add shared UI components

---
+++ src/components/consent-manager-client.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { ClientSideOptionsProvider } from "@c15t/nextjs/client"
+
+export function ConsentManagerClient({
+ children,
+}: {
+ children: React.ReactNode
+}) {
+ return <ClientSideOptionsProvider>{children}</ClientSideOptionsProvider>
+}
